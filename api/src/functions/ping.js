6593143494fc18be705c4bd870f45fd955933228
Jaller123 <jallerken@hotmail.se>
2025-11-13 12:13:57 +0100

    update

--- api/src/functions/ping.js
+++ api/src/functions/ping.js
@@ -1,5 +1,9 @@
 const { app } = require("@azure/functions");
+
 app.http("ping", {
-  methods: ["GET"], authLevel: "anonymous",
-  handler: async () => ({ jsonBody: { ok: true } })
+  methods: ["GET"],
+  authLevel: "anonymous",
+  handler: async (req, ctx) => {
+    return { jsonBody: { ok: true, message: "pong" } };
+  },
 });
